738e448e5b998ed002d774831d11df40b21bd6a0
Rozin Naufal Thoriq <rozin759@gmail.com>
2022-05-24 01:21:45 +0700

    update package result

---
+++ Android/ReCam App/app/src/main/java/com/dhandev/recam/ui/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.dhandev.recam.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dhandev.recam.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
